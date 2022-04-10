0381337a524cec8ed18359c361808a1b6063c2af
Nelson Ntepp <73397655+Neruso973@users.noreply.github.com>
2022-04-11 00:28:09 +0200

    routing done (#1)
    Co-authored-by: nelson.ntepp <nelson.ntepp@guarani.fr>

---
+++ front-end/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+
+import { BeerListComponent } from './beer-list/beer-list.component';
+
+const ROUTES: Routes = [{ path: 'beer-list', component: BeerListComponent }];
+
+export { ROUTES };
