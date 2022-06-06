d434d965a63cf31d2df5ef8066ac99e3967fb90b
nelson.ntepp <nelson.ntepp@guarani.fr>
2022-06-06 18:14:25 +0200

    update front rest api

--- front-end/src/app/services/beers.service.ts
+++ front-end/src/app/services/beers.service.ts
@@ -15,21 +15,21 @@ export class BeersService {
 
   public getBeers(): Observable<Array<beers>> {
     const beers: Observable<any> = this.service.get(
-      'http://localhost:3333/beers'
+      'http://localhost:3333/api/beers'
     );
     return beers;
   }
 
   public getBeerById(id: number): Observable<beers> {
     const beer: Observable<any> = this.service.get(
-      `http://localhost:3333/beers/${id}`
+      `http://localhost:3333/api/beers/${id}`
     );
     return beer;
   }
 
   public addBeers(beer: beers): Observable<beers> {
     const newBeer: Observable<any> = this.service.post(
-      'http://localhost:3333/beers/',
+      'http://localhost:3333/api/beers/',
       beer
     );
     return newBeer;
@@ -37,7 +37,7 @@ export class BeersService {
 
   public editBeer(id: number, beer: beers): Observable<beers> {
     const updateBeer: Observable<any> = this.service.put(
-      `http://localhost:3333/beers/${id}`,
+      `http://localhost:3333/api/beers/${id}`,
       beer
     );
     return updateBeer;
@@ -45,7 +45,7 @@ export class BeersService {
 
   public deleteBeer(id: number): Observable<beers> {
     const deletedBeer: Observable<any> = this.service.delete(
-      `http://localhost:3333/beers/${id}`
+      `http://localhost:3333/api/beers/${id}`
     );
     return deletedBeer;
   }
